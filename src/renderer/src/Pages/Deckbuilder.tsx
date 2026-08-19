024b5262f73f3ea077b919332c823188d5400200
Edward Dorta <dortaedward@gmail.com>
2026-08-19 09:50:09 -0400

    working on deck view

--- src/renderer/src/Pages/Deckbuilder.tsx
+++ src/renderer/src/Pages/Deckbuilder.tsx
@@ -1,9 +1,27 @@
-import { Check, Edit2, ShoppingCart, Upload } from "lucide-react";
+import { Check, ChevronDown, ChevronRight, Edit2, ShoppingCart, Upload } from "lucide-react";
 import { useState } from "react";
 
 export default function Deckbuilder() {
     const [inputToggle, setInputToggle] = useState(true);
     const [deckName, setDeck] = useState('Deck name');
+    const decks = [
+        {
+            name: "Main Deck",
+            cards: []
+        },
+        {
+            name: "Extra Deck",
+            cards: []
+        },
+        {
+            name: "Side Deck",
+            cards: []
+        },
+    ]
+
+    const [mainDeckOpen, setMainDeckOpen] = useState(false);
+
+
     return (
         <div className="flex h-full">
             <div className="flex-1 h-full min-h-0 overflow-y-auto text-white">
@@ -29,19 +47,87 @@ export default function Deckbuilder() {
                     </div>
                 </section>
                 {/* Deck Stats */}
-                <section className="border-y mx-4 border-border flex">
+                <section className="border-t mx-4 border-border flex">
                     <div className="border-r h-full border-border pr-4 flex flex-col  py-2">
                         <span className="text-muted text-xs font-light">MAIN DECK</span>
                         <span className="text-sky-500 font-bold">40</span>
                     </div>
                     <div className="border-r h-full border-border px-4 flex flex-col  py-2">
                         <span className="text-muted text-xs">EXTRA DECK</span>
-                        <span className="text-sky-500 font-bold">15</span>
+                        <span className="text-gray-50 font-bold">15</span>
                     </div>
                     <div className="border-r h-full border-border px-4 flex flex-col  py-2">
                         <span className="text-muted text-xs">SIDE DECK</span>
-                        <span className="text-sky-500 font-bold">15</span>
+                        <span className="text-gray-50 font-bold">15</span>
                     </div>
+                    <div className="border-r h-full border-border px-4 flex flex-col  py-2">
+                        <span className="text-muted text-xs">MONSTERS</span>
+                        <span className="text-gray-50 font-bold">15</span>
+                    </div>
+                    <div className="border-r h-full border-border px-4 flex flex-col  py-2">
+                        <span className="text-muted text-xs">SPELLS</span>
+                        <span className="text-gray-50 font-bold">15</span>
+                    </div>
+                    <div className="border-r h-full border-border px-4 flex flex-col  py-2">
+                        <span className="text-muted text-xs">TRAPS</span>
+                        <span className="text-gray-50 font-bold">15</span>
+                    </div>
+                </section>
+                <section className="bg-surface border-y border-border px-4 flex items-center justify-between">
+                    <div className="flex">
+                        <div className="h-full border-b-2 hover:bg-white/5 cursor-pointer border-blue-500 px-4 py-2">
+                            <span className="text-xs">Deck</span>
+                        </div>
+                        <div className="h-full hover:bg-white/5 cursor-pointer px-4 py-2">
+                            <span className="text-xs">Notes</span>
+                        </div>
+                        <div className="h-full hover:bg-white/5 cursor-pointer px-4 py-2">
+                            <span className="text-xs mr-2">Combos</span>
+                            <span className="text-[10px]">7</span>
+                        </div>
+                        <div className="h-full hover:bg-white/5 cursor-pointer px-4 py-2">
+                            <span className="text-xs mr-2">Comments</span>
+                            <span className="text-[10px]">12</span>
+                        </div>
+                    </div>
+                    <div>
+                        <button className="text-xs border border-border cursor-pointer px-3 py-1 rounded bg-white/5">+ Add cards</button>
+                    </div>
+                </section>
+                <section className="px-4 py-4">
+                    {
+                        decks.map(deck => {
+                            return (
+                                <div className="flex flex-col gap-2">
+                                    <div className="flex items-center justify-between border-b border-border">
+                                        <div className="cursor-pointer" onClick={() => setMainDeckOpen(prev => !prev)}>
+                                            <div className="flex items-center gap-4">
+                                                {
+                                                    mainDeckOpen
+                                                        ? <ChevronDown size={12} />
+                                                        : <ChevronRight size={12} />
+                                                }
+                                                <span className="text-xs">{deck.name} · MONSTERS <span>{deck.cards.length}/40</span></span>
+                                                <div className="relative bg-muted h-1 w-64">
+                                                    <div className="absolute top-0 left-0 bg-sky-600 h-1 w-2"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <span className="text-xs text-muted">12 unique</span>
+                                        </div>
+                                    </div>
+                                    {
+                                        mainDeckOpen
+                                        &&
+                                        <div className="py-2">
+                                            Cards
+                                        </div>
+                                    }
+                                </div>
+                            )
+                        })
+                    }
 
                 </section>
             </div>
